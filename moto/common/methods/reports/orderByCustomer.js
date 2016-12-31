966bbe30bceb13c732e29ad41576c104a489538e
yousour <hengyousour@gmail.com>
2016-12-31 10:03:19 +0700

    order by customer report

---
+++ moto/common/methods/reports/orderByCustomer.js
@@ -0,0 +1,136 @@
+import {Meteor} from 'meteor/meteor';
+import {ValidatedMethod} from 'meteor/mdg:validated-method';
+import {SimpleSchema} from 'meteor/aldeed:simple-schema';
+import {CallPromiseMixin} from 'meteor/didericis:callpromise-mixin';
+import {_} from 'meteor/erasaur:meteor-lodash';
+import {moment} from  'meteor/momentjs:moment';
+
+// Collection
+import {Company} from '../../../../core/common/collections/company';
+import {Branch} from '../../../../core/common/collections/branch';
+import {Exchange} from '../../../../core/common/collections/exchange';
+import {Customer} from '../../collections/customer';
+import {Order} from '../../collections/order';
+
+export const orderByCustomerReport = new ValidatedMethod({
+    name: 'moto.orderByCustomerReport',
+    mixins: [CallPromiseMixin],
+    validate: null,
+    run(params) {
+        if (!this.isSimulation) {
+            Meteor._sleepForMs(100);
+
+            let rptTitle, rptHeader, rptContent, rptFooter;
+
+            let fDate = moment(params.repDate[0]).startOf('day').toDate();
+            let tDate = moment(params.repDate[1]).endOf('day').toDate();
+
+            // --- Title ---
+            rptTitle = Company.findOne();
+
+            // --- Header ---
+            // Branch
+            let branchDoc = Branch.find({_id: {$in: params.branchId}});
+            params.branchHeader = _.map(branchDoc.fetch(), function (val) {
+                return `${val._id} : ${val.enName}`;
+            });
+
+            // Exchange
+            // let exchangeDoc = Exchange.findOne(params.exchangeId);
+            // params.exchangeHeader = JSON.stringify(exchangeDoc.rates, null, ' ');
+            params.exchangeHeader = "KHR";
+
+
+            // Customer
+            let customerDoc = Customer.findOne(params.customerId);
+            params.customerHeader = `${params.customerId} : ${customerDoc.name}`;
+            rptHeader = params;
+
+            // --- Content ---
+            let selector = {
+                branchId: {$in: params.branchId},
+                customerId:  params.customerId,
+                orderDate: {$gte: fDate, $lte: tDate}
+            };
+
+            rptContent = Order.aggregate([
+                {
+                    $match: selector
+                },
+                {
+                    $lookup: {
+                        from: "moto_customer",
+                        localField: "customerId",
+                        foreignField: "_id",
+                        as: "customerDoc"
+                    }
+                },
+                {
+                    $unwind: "$customerDoc"
+                },
+                {
+                    $lookup: {
+                        from: "core_branch",
+                        localField: "branchId",
+                        foreignField: "_id",
+                        as: "branchDoc"
+                    }
+                },
+                {
+                    $unwind: "$branchDoc"
+                },
+                {$sort: {orderDate: -1}},
+                {
+                    $group: {
+                        _id: {
+                            day: {$dayOfMonth: "$orderDate"},
+                            month: {$month: "$orderDate"},
+                            year: {$year: "$orderDate"},
+                            branchId: "$branchId"
+                        },
+                        orderDate: {$last: "$orderDate"},
+                        branchDoc: {$last: "$branchDoc"},
+                        total: {$sum: "$total"},
+                        discountAmount: {$sum: "$discountAmount"},
+                        subTotal: {$sum: "$subTotal"},
+                        dataOrder: {$push: "$$ROOT"},
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        branchId: "$_id.branchId",
+                        branchDoc: 1,
+                        orderDate: 1,
+                        subTotal: 1,
+                        discountAmount: 1,
+                        total: 1,
+                        dataOrder: 1
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$branchId",
+                        orderDate: {$last: "$orderDate"},
+                        branchDoc: {$last: "$branchDoc"},
+                        subTotal: {$sum: "$subTotal"},
+                        total: {$sum: "$total"},
+                        discountAmount: {$sum: "$discountAmount"},
+                        dataDate: {$push: "$$ROOT"}
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        subTotal: {$sum: "$subTotal"},
+                        discountAmount: {$sum: "$discountAmount"},
+                        total: {$sum: "$total"},
+                        dataBranch: {$push: "$$ROOT"}
+                    }
+                }
+            ])[0];
+     
+            return {rptTitle, rptHeader, rptContent};
+        }
+    }
+});
